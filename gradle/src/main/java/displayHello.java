4f65cec2daf12d9779276cf0d524b5133de1c63b
parineetajn <parineeta.jain@tothenew.com>
2020-02-26 17:23:14 +0530

    gradle

---
+++ gradle/src/main/java/displayHello.java
@@ -0,0 +1,5 @@
+public class displayHello {
+    public static void main(String[] args) {
+        System.out.println("Hello World ");
+    }
+}
---
+++ gradle/src/main/java/timetest.java
@@ -0,0 +1,9 @@
+import org.joda.time.LocalDate;
+
+public class timetest {
+    public static void main(String[] args) {
+
+        LocalDate localDate=new LocalDate(LocalDate.now());
+        System.out.println(localDate);
+    }
+}
